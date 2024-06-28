9a72e312076cb93de115bb51f3b903157fcebc0d
claudia.abad <clau.abad@gmail.com>
2024-06-28 09:23:07 -0400

    scheduling every 2 hours

--- mod_main.py
+++ mod_main.py
@@ -1,3 +1,6 @@
+import schedule
+from schedule import every, repeat
+import time as tm
 from tools.mod_helpers import transform_date, get_last_page, get_html_from_url
 from models.mood_classes import Institution, Situation
 from tools.mod_dao import insert_or_update_into_db, create_db_and_table
@@ -14,68 +17,76 @@ params = {
 
 create_db_and_table()
 
-soup = get_html_from_url(base_url, params)
-last_page = int(get_last_page(soup))
-print(f"Last page: {last_page}")
-
-
-for page_number in range(1, last_page + 1):
-    params['tx_solr[page]'] = page_number
-    print(f"Page number: {page_number}")
-
+def job():
+    params['tx_solr[page]'] = 1
     soup = get_html_from_url(base_url, params)
+    last_page = int(get_last_page(soup))
+    print(f"Last page: {last_page}")
 
-    if len(soup) > 0 :
 
-        last_update = transform_date(soup.find('div', class_='last-update-info').find_all("span")[1].text)
-        print(f"Last update: {last_update}")
+    for page_number in range(1, last_page + 1):
+        params['tx_solr[page]'] = page_number
+        print(f"Page number: {page_number}")
 
-        hospitals = soup.find('ul', class_='results-list list-group').find_all('div', class_='hospital_element')
+        soup = get_html_from_url(base_url, params)
 
-        for hospital in hospitals:
-            data_hospital = hospital.find(name='ul', class_='hospital-info')
-            title = data_hospital.find('li', class_='title')
-            name = title.find('div', class_='font-weight-bold').text
+        if len(soup) > 0 :
 
-            address = title.find('div', class_='adresse')
+            last_update = transform_date(soup.find('div', class_='last-update-info').find_all("span")[1].text)
+            print(f"Last update: {last_update}")
 
-            address_details = address.contents[0].strip()
-            street = address_details.split(',')[0]
-            city = address_details.split(',')[1]
-            code_postal = address_details.split(',')[2]
-            region = address.contents[2].strip()
+            hospitals = soup.find('ul', class_='results-list list-group').find_all('div', class_='hospital_element')
 
-            fiche = title.find('div', class_='lien-fiche-sante')
-            id = int(fiche.find('a').get("href").split('?nofiche=')[1])
+            for hospital in hospitals:
+                data_hospital = hospital.find(name='ul', class_='hospital-info')
+                title = data_hospital.find('li', class_='title')
+                name = title.find('div', class_='font-weight-bold').text
 
-            print(f"id: {id}")
-            print(f"Name: {name}")
-            print(f"Street: {street}")
-            print(f"City: {city}")
-            print(f"Postal Code: {code_postal}")
-            print(f"Region: {region}")
+                address = title.find('div', class_='adresse')
 
-            items = data_hospital.find_all('li', class_='hopital-item')
+                address_details = address.contents[0].strip()
+                street = address_details.split(',')[0]
+                city = address_details.split(',')[1]
+                code_postal = address_details.split(',')[2]
+                region = address.contents[2].strip()
 
-            wait_non_priority = items[0].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            waiting_to_see_doctor = items[1].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            total_people = items[2].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            occupancy_rate = items[3].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            avg_wait_room = items[4].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            avg_wait_stretcher = items[5].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
-            print(f"Estimated wait time for non-priority cases: {wait_non_priority}")
-            print(f"Number of people waiting to see a doctor: {waiting_to_see_doctor}")
-            print(f"Total number of people in the emergency room: {total_people}")
-            print(f"Occupancy rate of stretchers: {occupancy_rate}")
-            print(f"Average stay time in the waiting room (previous day): {avg_wait_room}")
-            print(f"Average stay time on a stretcher (previous day): {avg_wait_stretcher}")
+                fiche = title.find('div', class_='lien-fiche-sante')
+                id = int(fiche.find('a').get("href").split('?nofiche=')[1])
 
-            institution = Institution(institution_id=id, name=name, street=street, city=city, postal_code=code_postal,region=region)
-            situation = Situation(institution_id=id, wait_non_priority=wait_non_priority, waiting_to_see_doctor=waiting_to_see_doctor, total_people=total_people,
-                                  occupancy_rate=occupancy_rate, avg_wait_room=avg_wait_room, avg_wait_stretcher=avg_wait_stretcher, last_update=last_update)
+                print(f"id: {id}")
+                print(f"Name: {name}")
+                print(f"Street: {street}")
+                print(f"City: {city}")
+                print(f"Postal Code: {code_postal}")
+                print(f"Region: {region}")
 
-            insert_or_update_into_db(institution, situation)
-            print('-' * 40)
+                items = data_hospital.find_all('li', class_='hopital-item')
 
-    else:
-        print("Failed to retrieve the webpage")
+                wait_non_priority = items[0].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                waiting_to_see_doctor = items[1].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                total_people = items[2].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                occupancy_rate = items[3].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                avg_wait_room = items[4].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                avg_wait_stretcher = items[5].find_all('div')[1].find('span', class_='font-weight-bold').text.strip()
+                print(f"Estimated wait time for non-priority cases: {wait_non_priority}")
+                print(f"Number of people waiting to see a doctor: {waiting_to_see_doctor}")
+                print(f"Total number of people in the emergency room: {total_people}")
+                print(f"Occupancy rate of stretchers: {occupancy_rate}")
+                print(f"Average stay time in the waiting room (previous day): {avg_wait_room}")
+                print(f"Average stay time on a stretcher (previous day): {avg_wait_stretcher}")
+
+                institution = Institution(institution_id=id, name=name, street=street, city=city, postal_code=code_postal,region=region)
+                situation = Situation(institution_id=id, wait_non_priority=wait_non_priority, waiting_to_see_doctor=waiting_to_see_doctor, total_people=total_people,
+                                      occupancy_rate=occupancy_rate, avg_wait_room=avg_wait_room, avg_wait_stretcher=avg_wait_stretcher, last_update=last_update)
+
+                insert_or_update_into_db(institution, situation)
+                print('-' * 40)
+
+        else:
+            print("Failed to retrieve the webpage")
+
+schedule.every(2).hours.do(job)
+
+while True:
+    schedule.run_pending()
+    tm.sleep(1)
